e713f1317697bc922510e3d081ec291b93e54edb
sgca85 <162612350+sgca85@users.noreply.github.com>
2025-05-05 09:55:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int numero;
+bool paridade;
+
+Console.Write("Digite um numero: ");
+numero = int.Parse(Console.ReadLine()!);
+
+paridade = (numero % 2) == 0;
+
+if(paridade){
+    Console.WriteLine($"{numero} é par");
+}
+else{
+    Console.WriteLine($"{numero} é impar");
+}
